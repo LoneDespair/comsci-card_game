de6ca2c89310efedfe514e8f156b08896a31cdac
LoneDespairYt <lonedespairyt@gmail.com>
2022-11-03 01:50:19 +0800

    letters now goes to answer_logic

--- src/card/answer_logic.gd
+++ src/card/answer_logic.gd
@@ -2,6 +2,8 @@ extends Node
 
 const HOLDER := preload("./holder.tscn")
 
+var next_idx := 0
+
 onready var answer_container := $"%answer"
 onready var initiator := $"../initiator"
 
@@ -14,4 +16,13 @@ func setup(key : String) -> void:
 	for idx in key.length():
 		var holder := HOLDER.instance()
 		answer_container.add_child(holder)
+
+
+func add(letter_scn : Button) -> void:
+	var holder := answer_container.get_child(next_idx)
+	holder.add_child(letter_scn)
+	
+	next_idx += 1
+	
+	
 
--- src/card/selection_logic.gd
+++ src/card/selection_logic.gd
@@ -1,17 +1,23 @@
 extends Node
 
 onready var selection_container := $"%selection"
+onready var answer_container := $"%answer"
+onready var answer_logic := $"../answer_logic"
 
 
 func setup() -> void:
 	for letter_scn in selection_container.get_children():
-		letter_scn.connect("pressed", self, "select")
+		letter_scn.connect("pressed", self, "select", [letter_scn])
+
+
+func select(letter_scn : Button) -> void:
+	if answer_logic.next_idx < answer_container.get_child_count():
+		selection_container.remove_child(letter_scn)
+		letter_scn.disconnect("pressed", self, "select")
+		
+		answer_logic.add(letter_scn)
 	
-
-
-func select(letter_scn) -> void:
-	pass
-
-
+	else:
+		prints("Answer fulll")
 
 
