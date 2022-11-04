fe8f973b368572825e238fe0006fcb6d3c98a9a9
LoneDespairYt <lonedespairyt@gmail.com>
2022-11-05 05:44:49 +0800

    added entrace animation to card

--- src/play/initiator.gd
+++ src/play/initiator.gd
@@ -1,30 +1,8 @@
 extends Node
 
-const CARD_PCK := preload("../card/card.tscn")
-
-onready var deck := $"%deck"
-onready var turn := $"../turn"
 onready var exit_button := $"%exit"
-
-var key_list := ["COCO", "COCO", "DONUT", "JELLY", "CAKE"]
 
 
 func _ready() -> void:
 	exit_button.connect("pressed", owner, "queue_free")
-	
-	for idx in key_list.size():
-		var key = key_list[idx]
-		var card_scn := CARD_PCK.instance()
-		deck.add_child(card_scn)
-		
-#		card_scn.get_node("%question").text = key
-		
-		var checker := card_scn.get_node("checker")
-		checker.setup(key)
-	
-	turn.next()
-
-
-
-
 
--- src/play/turn.gd
+++ src/play/turn.gd
@@ -1,37 +1,57 @@
 extends Node
 
-var current_idx := -1
+const CARD_PCK := preload("../card/card.tscn")
+
+var card_scn : Control
+
+var key_table := {
+	"LOGIC" : "The basis of all mathematical reasoning, and of all automated reasoning",
+	"PROOF" : "Valid arguments that establish the truth of mathematical statements",
+	"OAK" : "Java was created by James Gosling, it was initially called ___, in honor of a tree",
+	"ITERATION" : "A continue statement makes the execution jump to the next _________ of the loop",
+	"CLOUD" : "A model for enabling convenient, on-demand network access to a shared pool of configurable computing resources",
+	"CLASS" : "A template in java used to create objects and to define object data types and methods",
+	"CONSTANT" : "A value that should not be altered by the program during normal execution",
+	"INTEGER" : "A number without a fractional component, and don't support decimal points",
+	"STRING" : "A datatype that contains an array of characters",
+	"BITWISE" : "a level of operation that involves working with individual bits",
+	"ARRAY" : "Collection of elements stored contigously in memory",
+	"BYTECODE" : "The resulting compiled object code of a Java program is called?",
+}
+
+var key_list := key_table.keys()
 
 onready var deck := $"%deck"
 onready var circular_initiator := $"%circular_timer/initiator"
 onready var fire := $"%fire"
 
 
-func next() -> void:
-	var card_count := deck.get_child_count()
+func _ready() -> void:
+	randomize()
+	key_list.shuffle()
 	
-	if card_count <= 0:
-		return
+	next()
+
+
+func next() -> void:
+	if is_instance_valid(card_scn):
+		prints("Animate an exit sequence")
+	
+	card_scn = CARD_PCK.instance()
+	deck.add_child(card_scn)
+	
+	var key = key_list.pop_back()
+	card_scn.get_node("%question").text = key_table[key]
+	card_scn.get_node("checker").setup(key)
 	
 	circular_initiator.start()
+	var card_initiator := card_scn.get_node("initiator")
 	
-	if current_idx > -1:
-		deck.get_child(current_idx).get_node("initiator").set_front(false)
+	circular_initiator.connect("timeout", card_initiator, "timeout")
+	card_scn.get_node("checker").connect("correct", self, "correct")
+	card_scn.get_node("%next").connect("pressed", self, "next")
 	
-	
-	if current_idx + 1 < card_count:
-		current_idx += 1
-		var card_scn := deck.get_child(current_idx) as Control
-		var card_initiator := card_scn.get_node("initiator")
-		
-		circular_initiator.connect("timeout", card_initiator, "timeout")
-		card_scn.get_node("checker").connect("correct", self, "correct")
-		
-		card_scn.get_node("%next").connect("pressed", self, "next")
-		deck.rect_position.x -= card_scn.rect_size.x + 5
-	
-	else:
-		prints("Gameover")
+#	deck.rect_position.x -= card_scn.rect_size.x + 5
 
 
 func correct() -> void:
