f87cf29527bb946a6d95adba98213584cd6a7347
LoneDespairYt <lonedespairyt@gmail.com>
2022-11-11 23:20:28 +0800

    letters are now centered

--- src/card/selection_logic.gd
+++ src/card/selection_logic.gd
@@ -1,6 +1,9 @@
 extends Node
 
-onready var selection_container := $"%selection"
+const MAX_COLUMNS := 9
+const MIN_COLUMNS := 2
+
+onready var selection_container := $"%selection" as GridContainer
 onready var answer_container := $"%answer"
 onready var answer_logic := $"../answer_logic"
 
@@ -12,6 +15,7 @@ func _ready() -> void:
 func add(letter_scn : Button) -> void:
 	letter_scn.connect("pressed", self, "select", [letter_scn])
 	selection_container.add_child(letter_scn)
+	balance()
 
 
 func select(letter_scn : Button) -> void:
@@ -22,8 +26,17 @@ func select(letter_scn : Button) -> void:
 		letter_scn.disconnect("pressed", self, "select")
 		
 		answer_logic.add(letter_scn, holder)
+		balance()
 	
 	else:
 		prints("Answer fulll")
 
+
+func balance() -> void:
+	var count : float = selection_container.get_child_count()
+	
+	if count > MIN_COLUMNS:
+		selection_container.columns = (count + 1) / 2
+		prints(selection_container.columns)
+	
 
